9d03e2d139d90d16da45a90a5b4ed623243e4180
Adam Freiesleben <adam.fre@seznam.cz>
2022-05-23 14:53:59 +0200

    Global state with Vuex and fetching method

--- src/components/Home/SearchForm.vue
+++ src/components/Home/SearchForm.vue
@@ -2,7 +2,7 @@
   <section class="mt-4">
     <div class="container">
       <form
-        @submit.prevent
+        @submit.prevent="fetchRepos"
         class="p-2 bg-white rounded-1 shadow d-flex flex-column flex-sm-row align-items-center"
       >
         <label
@@ -15,16 +15,17 @@
           <input
             id="searchbar"
             type="text"
-            class="border-0 shadow-none px-3"
+            class="border-0 shadow-none p-3 w-100 text-dark fw-medium"
             placeholder="Search GitHub username..."
             spellcheck="false"
             v-model="searchText"
+            autocomplete="off"
           />
         </label>
         <button
-          class="btn btn-primary text-primary-50 fw-medium shadow-none py-3 px-5"
+          class="btn btn-primary text-primary-50 fw-bold shadow-none py-3 px-5"
         >
-          Find repos {{ searchText }}
+          Find repos
         </button>
       </form>
     </div>
@@ -38,6 +39,15 @@ export default {
     return {
       searchText: "",
     };
+  },
+  methods: {
+    fetchRepos() {
+      this.$store.dispatch("fetchRepos", {
+        username: this.searchText,
+      });
+
+      console.log(this.$store.repos);
+    },
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,59 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
-  state: {},
+  state: {
+    repos: [],
+    loading: false,
+    error: null,
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    setRepos(state, payload) {
+      state.repos = payload;
+    },
+    setLoading(state, payload) {
+      state.loading = payload;
+    },
+    setError(state, payload) {
+      state.error = payload;
+    },
+  },
+  actions: {
+    fetchRepos: async ({ commit }, { username }) => {
+      commit("setLoading", true);
+      commit("setError", null);
+
+      try {
+        const { data } = axios.get(
+          `https://api.github.com/users/${username}/repos`
+        );
+
+        commit("setRepos", data);
+        commit("setLoading", false);
+      } catch (error) {
+        switch (error.response.status) {
+          case 404:
+            commit("setError", "Couldn't find this GitHub user");
+            break;
+          case 403:
+            commit(
+              "setError",
+              "You don't have permission to view this user's repos"
+            );
+            break;
+          case 401:
+            commit(
+              "setError",
+              "You need to be authorized to view this user's repos"
+            );
+            break;
+          default:
+            commit("setError", "Something went wrong");
+            break;
+        }
+      }
+    },
+  },
   modules: {},
 });
