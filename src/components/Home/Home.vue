1182816ed4954eca6a2eae87854244a9e4a1e404
Adam Freiesleben <adam.fre@seznam.cz>
2022-05-23 14:12:20 +0200

    Home component start

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <div class="container">HOME</div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,15 @@
 <template>
   <Layout>
-    <div class="home"></div>
+    <Home />
   </Layout>
 </template>
 
 <script>
 import Layout from "@/components/Layout";
+import Home from "@/components/Home";
 
 export default {
   name: "HomeView",
-  components: { Layout },
+  components: { Layout, Home },
 };
 </script>
