09617458b67b4a9b6100f4c321aaca2ade79b7e1
Adam Freiesleben <adam.fre@seznam.cz>
2022-05-23 17:33:16 +0200

    Repos items title, description, owner name and updated ago

--- src/components/Home/Repos/RepoItem.vue
+++ src/components/Home/Repos/RepoItem.vue
@@ -1,7 +1,16 @@
 <template>
-  <li class="col-sm-6 col-md-4 col-lg-3 p-2">
-    <div class="repo p-2 bg-white rounded-2 shadow">
-      {{ repo.name }}
+  <li class="col-sm-6 col-md-4 col-lg-3 p-3">
+    <div class="repo p-2 bg-white rounded-3 shadow d-flex flex-column p-4">
+      <h3 class="fs-4 fw-semibold text-gray-600 text-capitalize">
+        {{ nameWithoutDashes }}
+      </h3>
+      <h4 class="fs-6 fw-semibold font-mono text-primary">
+        {{ repo.owner.login }}
+      </h4>
+      <p class="text-gray-500 fs-7 fw-medium flex-grow-1 pt-3">
+        {{ repo.description ? repo.description : "No description" }}
+      </p>
+      <p class="text-gray-400 fw-medium m-0 fs-6">Updated {{ updatedAgo }}</p>
     </div>
   </li>
 </template>
@@ -10,14 +19,30 @@
 export default {
   name: "RepoItem",
   props: ["repo"],
+  computed: {
+    updatedAgo() {
+      const ta = require("time-ago");
+      return ta.ago(this.repo.updated_at);
+    },
+    nameWithoutDashes() {
+      return this.repo.name.replace("-", " ");
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 li {
   list-style: none;
-}
-.repo {
-  min-height: 200px;
+
+  .repo {
+    min-height: 16rem;
+    transition: 120ms ease;
+    cursor: pointer;
+
+    &:hover {
+      transform: scale(1.02);
+    }
+  }
 }
 </style>
--- src/components/Home/Repos/ReposList.vue
+++ src/components/Home/Repos/ReposList.vue
@@ -2,7 +2,11 @@
   <section class="my-5">
     <div class="container">
       <ul class="row p-0">
-        <RepoItem v-for="repo in repos" :key="repo.id" :repo="repo" />
+        <RepoItem
+          v-for="repo in recentlyUpdatedRepos"
+          :key="repo.id"
+          :repo="repo"
+        />
       </ul>
     </div>
   </section>
@@ -15,5 +19,12 @@ export default {
   name: "ReposList",
   props: ["repos"],
   components: { RepoItem },
+  computed: {
+    recentlyUpdatedRepos() {
+      return [...this.repos].sort((a, b) =>
+        b.updated_at.localeCompare(a.updated_at)
+      );
+    },
+  },
 };
 </script>
