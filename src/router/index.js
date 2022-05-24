811794fda3a4242b47481bbfd1a3e11e5a789697
Adam Freiesleben <adam.fre@seznam.cz>
2022-05-24 13:13:45 +0200

    Page titles

--- src/router/index.js
+++ src/router/index.js
@@ -7,18 +7,29 @@ const routes = [
     path: "/",
     name: "home",
     component: HomeView,
+    meta: {
+      title: "Find GitHub repositories",
+    },
   },
   // 404
   {
     path: "/:catchAll(.*)",
     name: "notFound",
     component: NotFoundView,
+    meta: {
+      title: "Page not found",
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`;
+  next();
 });
 
 export default router;
