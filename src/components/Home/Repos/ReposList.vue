3bf22288a11013dbfbb694a4d685fe1dd5ac857a
Adam Freiesleben <adam.fre@seznam.cz>
2022-05-31 16:48:11 +0200

    ReposList container class moved and second one removed

--- src/components/Home/Repos/ReposList.vue
+++ src/components/Home/Repos/ReposList.vue
@@ -1,21 +1,18 @@
 <template>
-  <section class="my-5">
-    <div class="container">
-      <ul v-if="repos.length" class="row p-0">
-        <RepoItem
-          v-for="repo in recentlyUpdatedRepos"
-          :key="repo.id"
-          :repo="repo"
-        />
-      </ul>
-      <div v-else class="container">
-        <p
-          class="text-center fs-4 text-primary-800 bg-white rounded shadow p-5 mt-5"
-        >
-          This user doesn't have any public repositories...
-        </p>
-      </div>
-    </div>
+  <section class="my-5 container">
+    <ul v-if="repos.length" class="row p-0">
+      <RepoItem
+        v-for="repo in recentlyUpdatedRepos"
+        :key="repo.id"
+        :repo="repo"
+      />
+    </ul>
+    <p
+      v-else
+      class="text-center fs-4 text-primary-800 bg-white rounded shadow p-5 mt-5"
+    >
+      This user doesn't have any public repositories...
+    </p>
   </section>
 </template>
 
