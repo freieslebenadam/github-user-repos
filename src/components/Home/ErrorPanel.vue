a84b266eef09ed6d7252c8cad9e6c14913411976
Adam Freiesleben <adam.fre@seznam.cz>
2022-05-31 16:57:49 +0200

    Better convention elements in error panel and loading panel

--- src/components/Home/ErrorPanel.vue
+++ src/components/Home/ErrorPanel.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="text-center my-5 container">
-    <div
+  <section class="text-center my-5 container">
+    <p
       class="error-message bg-white rounded-1 shadow py-4 text-danger fs-5 d-flex align-items-center justify-content-center fw-medium"
     >
       <span class="me-3 fs-4 text-danger d-none d-md-inline">
         <i class="bi bi-exclamation-triangle-fill"></i>
       </span>
       {{ error }}
-    </div>
-  </div>
+    </p>
+  </section>
 </template>
 
 <script>
--- src/components/Home/LoadingPanel.vue
+++ src/components/Home/LoadingPanel.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="mx-auto text-center my-5 container">
+  <section class="mx-auto text-center my-5 container">
     <div class="spinner-border text-primary" role="status">
       <span class="visually-hidden">{{ text ? text : "Loading" }}</span>
     </div>
     <h2 v-if="text" class="fs-3 fw-medium text-primary mt-2">{{ text }}</h2>
-  </div>
+  </section>
 </template>
 
 <script>
